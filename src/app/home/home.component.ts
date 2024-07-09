4435aaa9ab4b58c4c13cb7bba7544f3dda479af3
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-07-09 13:14:04 +0700

    [ADD] create simle home component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-home',
+  template: ` <p>This is home component</p> `,
+})
+export class HomeComponent {}
