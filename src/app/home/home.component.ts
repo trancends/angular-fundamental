d034a1d7e944410ee23841329cc4d24779a858f7
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-07-09 15:41:03 +0700

    [ADD] implementing @Pipe
    - @Pipe allows to create custom pipe to filter data
    - use it after '|' followed by 'name of the pipe'
    - creating reverse pipe to reverse a value

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { WelcomeComponent } from '../ui/welcome.component';
 import { RandomColor } from '../ui/random-color.directive';
+import { ReversePipe } from '../ui/reverse.pipe';
 
 @Component({
   standalone: true,
@@ -9,7 +10,10 @@ import { RandomColor } from '../ui/random-color.directive';
     <app-welcome />
     <p>This is home component</p>
     <p randomColor>I am stylish</p>
+    <p>Time for a little : {{ magic | reverse }}</p>
   `,
-  imports: [WelcomeComponent, RandomColor],
+  imports: [WelcomeComponent, RandomColor, ReversePipe],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  magic = ' reverse me ';
+}
