5bd225f838bd7760ac3c12299689eeae12248c3f
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2024-07-10 15:07:19 +0700

    [ADD] create route navigation with route param
    - create detail page with param id
    - whenever skill got clicked it will navigate to detail/id and passing
    its id
    - use routerLink to navigate when clicking an element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink],
 })
 export class AppComponent {
   title = 'my-app';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { WelcomeComponent } from '../ui/welcome.component';
 import { RandomColor } from '../ui/random-color.directive';
 import { ReversePipe } from '../ui/reverse.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [WelcomeComponent, RandomColor, ReversePipe],
+  imports: [WelcomeComponent, RandomColor, ReversePipe, RouterLink],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <app-welcome
@@ -19,9 +20,31 @@ import { ReversePipe } from '../ui/reverse.pipe';
     <p randomColor>{{ 'reverse this' | reverse }}</p>
     <p>This is home component</p>
     <p>{{ value }}</p>
+    <ul>
+      <h3>My Skills:</h3>
+      @for (skill of skills; track skill.id) {
+        <li>
+          <a routerLink="/detail/{{ skill.id }}">{{ skill.name }}</a>
+        </li>
+      }
+    </ul>
   `,
 })
 export class HomeComponent implements OnInit {
+  skills = [
+    {
+      id: 1,
+      name: 'Angular',
+    },
+    {
+      id: 2,
+      name: 'React',
+    },
+    {
+      id: 3,
+      name: 'Vue',
+    },
+  ];
   user = {
     name: 'Ben',
   };
